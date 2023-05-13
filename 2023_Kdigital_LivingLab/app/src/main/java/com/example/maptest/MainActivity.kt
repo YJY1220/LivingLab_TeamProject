275934ac4394af39c298492d0d3bb0c4f16757fd
YJY1220 <jiyeyu1220@gmail.com>
2023-05-13 15:30:54 +0900

    Removed files later

---
+++ 2023_Kdigital_LivingLab/app/src/main/java/com/example/maptest/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.maptest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+package com.example.maptest
+
+        mport androidx.accompat.app.AcppCompatActivity
+        import andorid.os.Bundle
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.acitivty._nmain)
+    }
+}
