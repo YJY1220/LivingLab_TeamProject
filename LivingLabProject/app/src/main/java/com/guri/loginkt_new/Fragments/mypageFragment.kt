208bb5d7f71d725ffb5dd73a5f1609f7697f4f79
Lee Sumin <lsmlsm426@knu.ac.kr>
2023-05-21 12:20:24 +0900

    update build.gradle

--- LivingLabProject/app/src/main/java/com/guri/loginkt_new/Fragments/mypageFragment.kt
+++ LivingLabProject/app/src/main/java/com/guri/loginkt_new/Fragments/mypageFragment.kt
@@ -6,6 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.guri.loginkt_new.R
+import com.guri.loginkt_new.databinding.FragmentMypageBinding
 
 class mypageFragment : Fragment(R.layout.fragment_mypage) {
+    private var _binding: FragmentMypageBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentMypageBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.btnLogin.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fragmentCotainer, homeFragment()).commit()
+        }
+
+        return view
+    }
 }
